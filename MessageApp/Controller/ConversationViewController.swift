675447ba34b9931d442c3230ee673a42c9803397
Bryan Battu <bryan.battu@gmail.com>
2024-04-13 10:38:38 +0200

    init

--- MessageApp/Controller/ConversationViewController.swift
+++ MessageApp/Controller/ConversationViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ConversationViewController.swift
 //  MessageApp
 //
 //  Created by Bryan Battu on 13/04/2024.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ConversationViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
